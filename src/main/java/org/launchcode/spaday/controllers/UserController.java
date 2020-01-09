744852a0c8a2968ddd003406b74791a9ae8dfe95
denysV1978 <denysvuk@gmail.com>
2020-01-08 21:50:24 -0600

    Studio spa validation is done

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,23 +3,35 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+
+        model.addAttribute("user", new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
+    public String processAddUserForm(Model model, String verify, @ModelAttribute @Valid User user, Errors errors) {
+        // do not forget that Errors errors should be after User user!!!! right away
+//        model.addAttribute("user", user);
+//        model.addAttribute("verify", verify);
+//        model.addAttribute("username", user.getUsername());
+//        model.addAttribute("email", user.getEmail());
+
+
+        if(errors.hasErrors()) {
+            //System.out.println("error");
+            return "user/add";
+        }
         if (user.getPassword().equals(verify)) {
            return "user/index";
         }
@@ -27,6 +39,7 @@ public class UserController {
             model.addAttribute("error", "Passwords do not match");
             return "user/add";
         }
+
 
     }
 
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,19 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotNull
+    @Size(min = 5, max = 15, message = "User name is too short or too long. Should be between 5 to 15 symbols")
     private String username;
+
+    @Email(message = "Should be email type")
     private String email;
+
+    @NotNull
+    @Size(min = 6, message = "Password should have minimum 6 symbols")
     private String password;
 
     public User() {
